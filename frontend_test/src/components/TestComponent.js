7d7b6d12a9da181fcabb751f32d16507f23100a1
MyungHo Lee <105653230+MyM999@users.noreply.github.com>
2022-10-27 15:36:17 +0900

    Add files via upload

---
+++ frontend_test/src/components/TestComponent.js
@@ -0,0 +1,18 @@
+// import React, { Component } from "react";
+// import api from '../services/api'
+// import TestService from '../services/'
+
+// export default function login(username, password) {
+//     return api
+//       .post("/auth/signin", {
+//         username,
+//         password
+//       })
+//       .then(response => {
+//         if (response.data.accessToken) {
+//           TokenService.setUser(response.data);
+//         }
+
+//         return response.data;
+//       });
+//   };
